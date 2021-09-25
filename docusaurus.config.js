57233c821cd1ddd7e6d5f591739cbed1e71062d1
peterdyer7 <peter_dyer@hotmail.com>
2021-09-25 12:11:36 -0400

    added bash highlighting to config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -95,6 +95,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         prism: {
           theme: lightCodeTheme,
           darkTheme: darkCodeTheme,
+          additionalLanguages: ['bash'],
         },
       }),
   }
